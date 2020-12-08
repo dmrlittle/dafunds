c43252cc9c715ce7638f2fcfe7968b75f85c1c59
MrLittle <62012580+dmrlittle@users.noreply.github.com>
2020-12-08 17:53:16 +0530

    salvation

--- dafunds/bunker/forms.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-
-class CreateVaultForm(FlaskForm):
-    name = StringField("Vault's Name",validators=[DataRequired(),Length(max=20)])
-    submit = SubmitField('Build')
--- run.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from dafunds import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
